6f20dd7d474713dc554cb43083885156adbaa3d1
Angel Terol <angel.terol@3xs.es>
2024-04-15 17:50:20 +0200

    test

--- app/src/main/java/es/ua/eps/opencv/MainActivity.kt
+++ app/src/main/java/es/ua/eps/opencv/MainActivity.kt
@@ -1,16 +1,24 @@
 package es.ua.eps.opencv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.SurfaceView
+import android.view.WindowManager
 import android.widget.Toast
-import org.opencv.android.NativeCameraView.TAG
+import org.opencv.android.CameraActivity
+import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.Mat
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : CameraActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private val TAG = "MainActivity"
+
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         if (OpenCVLoader.initLocal()) {
             Log.i(TAG, "OpenCV loaded successfully")
@@ -20,6 +28,46 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        setContentView(R.layout.activity_main)
+
+        mOpenCvCameraView = findViewById(R.id.surface_view) as CameraBridgeViewBase
+
+        mOpenCvCameraView!!.visibility = SurfaceView.VISIBLE
+
+        mOpenCvCameraView!!.setCvCameraViewListener(this)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mOpenCvCameraView?.disableView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mOpenCvCameraView?.enableView()
+    }
+
+    override fun getCameraViewList(): List<CameraBridgeViewBase?>? {
+        return listOf<CameraBridgeViewBase>(mOpenCvCameraView)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null) mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+
+    }
+
+    override fun onCameraViewStopped() {
+
+    }
+
+    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
+        return inputFrame!!.rgba()
     }
 }
